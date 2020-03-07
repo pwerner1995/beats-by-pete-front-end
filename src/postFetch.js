1ce3edc6a457687f9d042668331e518b6e903356
github username <github email address>
2020-03-07 12:03:31 -0500

    Clean up search results post function

--- src/postFetch.js
+++ src/postFetch.js
@@ -1,82 +1,9 @@
-import React from 'react';
-import { connect } from "react-redux"
+// import React from 'react';
+// import { connect } from "react-redux"
 import store from './store'
 
 
-// function postSongs(songs){
-//     console.log("SONGS: ", songs)
-//     songs.forEach((song) => {
-//         fetch("http://localhost:3000/api/v1/songs",{method: "POST",
-//             headers:{
-//                 "Content-Type": "application/json",
-//                 "Accept": "application/json",
-//                 "Access-Control-Allow-Origin": "http://localhost:3000",
-//                 'Access-Control-Allow-Credentials': 'true'
-//             },
-//             body: JSON.stringify({
-//                 song:{
-
-//                     title: song.title,
-//                     duration: song.duration,
-//                     preview: song.preview
-//                 }
-//             })
-//         })
-//         .then((resp) => {
-//             console.log(resp)
-//         })
-//     })
-// }
-// function postArtists(artists){
-//     console.log("ARTISTS: ", artists)
-//     artists.forEach((artist) => {
-//         fetch("http://localhost:3000/api/v1/artists",{method: "POST",
-//             headers:{
-//                 "Content-Type": "application/json",
-//                 "Accept": "application/json",
-//                 "Access-Control-Allow-Origin": "http://localhost:3000",
-//                 'Access-Control-Allow-Credentials': 'true'
-//             },
-//             body: JSON.stringify({
-//                 artist: {
-//                     name: artist.name,
-//                     picture: artist.picture
-//                 }
-//             })
-//         })
-//         .then((resp) => {
-//             console.log(resp)
-//         })
-//     })
-// }   
-// function postAlbums(albums){
-//     console.log("ALBUMS: ", albums)
-//     albums.forEach((a) => {
-//         fetch("http://localhost:3000/api/v1/albums",{method: "POST",
-//             headers:{
-//                 "Content-Type": "application/json",
-//                 "Accept": "application/json",
-//                 "Access-Control-Allow-Origin": "http://localhost:3000",
-//                 'Access-Control-Allow-Credentials': 'true'
-//             },
-//             body: JSON.stringify({
-//                 album:{
-//                     title: a.album.title,
-//                     cover: a.album.cover,
-//                     artist: a.artist
-//                 }
-//             })
-//         })
-//         .then((resp) => {
-//             console.log(resp)
-//         })
-//     })
-// }
-
-
-
 export function postSearchResults(results){
-    // console.log(props)
     const state =  store.getState()
     console.log("state: ",state)
     let songs = []
@@ -94,17 +21,7 @@ export function postSearchResults(results){
             }
         }
     })
-    // postArtists(artists)
-    // .then(postAlbums(albums))
-    // .then(postSongs(songs))
-    
+   
 }
 
-// function msp(state){
-//     return {
-//         artists: state.artists,
-//         songs: state.songs,
-//         albums: state.albums
-//     }
-// }
 
